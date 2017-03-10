d0aaf9da015138d58a9d814dc9b9993f67d12941
Matti Schneider <matti.schneider@beta.gouv.fr>
2017-03-10 18:49:28 +0100

    Add monitoring script

---
+++ monitor.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+MAX_DISK_USAGE=90  # percentage
+PARTITION_TO_MONITOR=root
+PORT=8000
+OPENFISCA_PORT=12000
+PROTOCOL=https
+PUBLIC_HOST=mes-aides.gouv.fr
+
+
+disk_usage=$(df | grep $PARTITION_TO_MONITOR | tr -s ' ' | cut -d ' ' -f 5 | tr -d '%')
+echo "$PARTITION_TO_MONITOR disk usage: $disk_usage%"
+
+failures=0
+
+if ! curl -sL -w "OpenFisca\t GET %{url_effective} -> %{http_code}\\n" http://localhost:$OPENFISCA_PORT -o /dev/null
+then let failures++
+fi
+
+if ! curl -sL -w "App (locally)\t GET %{url_effective} -> %{http_code}\\n" http://localhost:$PORT -o /dev/null
+then let failures++
+fi
+
+if ! curl -sL -w "App (on public internet)\t GET %{url_effective} -> %{http_code}\\n" $PROTOCOL://$PUBLIC_HOST -o /dev/null
+then let failures++
+fi
+
+if ! test $disk_usage -lt $MAX_DISK_USAGE
+then let failures++
+fi
+
+echo "$failures failed tests"
+
+exit $failures
