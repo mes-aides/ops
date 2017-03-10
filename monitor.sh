be78eb257821192bdede90c610baad4905c4d208
Matti Schneider <matti.schneider@beta.gouv.fr>
2017-03-10 18:58:58 +0100

    Log currently deployed version and last deploy

--- monitor.sh
+++ monitor.sh
@@ -8,11 +8,17 @@ PORT=8000
 OPENFISCA_PORT=12000
 PROTOCOL=https
 PUBLIC_HOST=mes-aides.gouv.fr
+DEPLOYED_DIRECTORY=/var/www/dds
 
 
-disk_usage=$(df | grep $PARTITION_TO_MONITOR | tr -s ' ' | cut -d ' ' -f 5 | tr -d '%')
-echo "$PARTITION_TO_MONITOR disk usage: $disk_usage%"
+echo "Last deployment: $(stat -c %y /home/deploy/production.ini)"
 
+cd $DEPLOYED_DIRECTORY
+echo "Deployed version (as given by $DEPLOYED_DIRECTORY):"
+git log --pretty=oneline -1
+
+echo
+echo "Smoke tests:"
 failures=0
 
 if ! curl -sL -w "OpenFisca\t GET %{url_effective} -> %{http_code}\\n" http://localhost:$OPENFISCA_PORT -o /dev/null
@@ -27,10 +33,13 @@ if ! curl -sL -w "App (on public internet)\t GET %{url_effective} -> %{http_code
 then let failures++
 fi
 
+disk_usage=$(df | grep $PARTITION_TO_MONITOR | tr -s ' ' | cut -d ' ' -f 5 | tr -d '%')
+echo "$PARTITION_TO_MONITOR disk usage: $disk_usage%"
 if ! test $disk_usage -lt $MAX_DISK_USAGE
 then let failures++
 fi
 
+echo
 echo "$failures failed tests"
 
 exit $failures
