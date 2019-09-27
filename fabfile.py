fca32077639b2b1c003ff720cb8e5b788f09d024
Thomas Guillet <guillet.thomas@gmail.com>
2019-09-27 14:18:15 +0200

    Prepare server and monitor endpoint

--- fabfile.py
+++ fabfile.py
@@ -12,7 +12,7 @@ loader = Environment(loader=FileSystemLoader('.'))
 
 
 @contextmanager
-def write(path, ctx):
+def write_template(path, ctx):
   fp = tempfile.TemporaryFile(mode='w+', encoding='utf8')
   t = loader.get_template(path)
   t.stream(**ctx).dump(fp)
@@ -22,18 +22,12 @@ def write(path, ctx):
 
 
 @task
-def tell_me_your_name(c, file='ok'):
-  print(file)
+def tell_me_your_name(c):
   cc = Connection(host=SERVER_IP, user=USER)
   cc.run('hostname')
   cc.run('date')
   cc.run('uname -a')
   cc.run('lsb_release -a')
-  cc.put('test', remote='/root/test')
-  cc.run('cat /root/test')
-  with write('test', {'name': 'Dan'}) as fp:
-    cc.put(fp, '/root/template')
-  cc.run('cat /root/template')
 
 
 @task
@@ -45,22 +39,39 @@ def bootstrap(c):
   python(cc)
   mongodb(c)
 
+
 @task
 def test(ctx):
   c = Connection(host=SERVER_IP, user=USER)
+  monitor(c)
 
 
 def nginx(c):
   c.run('apt-get install --assume-yes nginx')
+  c.put('files/nginx.upstreams.conf', '/etc/nginx/conf.d/upstreams.conf')
+  c.run('rm -f /etc/nginx/sites-enabled/default')
+  c.run('service nginx reload')
 
 
 def system(c):
-  c.run('apt-get install --assume-yes curl man')
+  c.run('apt-get install --assume-yes curl=7.52.1-5+deb9u9') # Curl versions conflict
+  c.run('apt-get install --assume-yes man')
+  usermain(c)
+
+
+def usermain(c):
+  c.run('useradd main --create-home')
 
 
 def node(c):
   c.run('curl --silent --location https://deb.nodesource.com/setup_12.x | bash -')
   c.run('apt-get install --assume-yes nodejs')
+  pm2(c)
+
+
+def pm2(c):
+  #c.run('npm install --global pm2@3.5.1')
+  c.run('pm2 startup systemd')
 
 
 def python(c):
@@ -79,3 +90,21 @@ def mongodb(c):
     print('MongoDB packages already setup')
   c.run('apt-get install --assume-yes libcurl3')
   c.run('apt-get install --assume-yes mongodb-org')
+
+
+def monitor(c):
+  c.run('mkdir --parents /opt/mes-aides')
+  c.put('files/monitor/monitor.sh', '/opt/mes-aides/monitor.sh')
+  c.put('files/monitor/monitor-server.js', '/opt/mes-aides/monitor-server.js')
+  c.put('files/monitor/ma-monitor.service', '/etc/systemd/system/ma-monitor.service')
+  c.run('systemctl daemon-reload')
+  c.run('service ma-monitor restart')
+  c.run('systemctl enable ma-monitor')
+  monitor = {
+    'name': 'monitor.mes-aides.gouv.fr',
+    'upstream_name' : 'monitor',
+    'webroot_path': '/var/www/',
+  }
+  with write_template('files/nginx_config.template', monitor) as fp:
+    c.put(fp, '/etc/nginx/sites-enabled/monitor.mes-aides.gouv.fr.conf')
+  c.run('service nginx reload')
